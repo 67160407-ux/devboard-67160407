c284ebf42fafc7ecf580d91355711a1df5571033
67160407 <67160407@go.buu.ac.th>
2026-03-29 18:44:36 +0700

    task3 fix

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -102,8 +102,7 @@ function PostList({ favorites = [], onToggleFavorite = () => {} }) {
       {filtered.map((post) => (
         <PostCard
           key={post.id}
-          title={post.title}
-          body={post.body}
+          post={post}
           isFavorite={favorites.includes(post.id)}
           onToggleFavorite={() => onToggleFavorite(post.id)}
         />
