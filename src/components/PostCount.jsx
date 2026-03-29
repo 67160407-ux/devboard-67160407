329357666473825be20923b60912ddbb0999e976
67160407 <67160407@go.buu.ac.th>
2026-03-29 18:01:10 +0700

    task2 act1

---
+++ src/components/PostCount.jsx
@@ -0,0 +1,9 @@
+function PostCount({ count }) {
+  return (
+    <div style={{ color: "#4a5568", marginBottom: "1rem", fontWeight: "500" }}>
+      โพสต์ทั้งหมด: {count} รายการ
+    </div>
+  );
+}
+
+export default PostCount;
